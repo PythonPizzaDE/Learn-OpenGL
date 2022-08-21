d3d47fe3d96f6954a2e4764402becc4e610cfcfe
Paul Frische <paul@paulfrische.xyz>
2022-08-21 11:54:22 +0200

    shaders ._.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+#include <fstream>
+#include <iostream>
+//
 // use (modern) OpenGL
 #define GLEW_STATIC
 #include <GL/glew.h>
@@ -9,12 +12,57 @@
 #include "util.h"
 
 
+const float vertecies[] = {
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f, 0.5f, 0.0f,
+};
+
+const char* vertexSource = R"glsl(
+#version 330 core
+
+layout (location = 0) in vec3 aPos;
+
+void main()
+{
+    gl_Position = vec4(aPos, 1.0);
+}
+)glsl";
+
+const char* fragmentSource =  R"glsl(
+#version 330 core
+
+out vec4 FragColor;
+
+void main()
+{
+    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+}
+)glsl";
+
 int main(int argc, char** argv)
 {
+
     GLFWwindow* window = setup();
+
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertecies), vertecies, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    GLuint shaderProgram = getShaderProgram(vertexSource, fragmentSource);
+    glUseProgram(shaderProgram);
 
     while (!glfwWindowShouldClose(window))
     {
+        glDrawArrays(GL_TRIANGLES, 0, 3);
         glfwSwapBuffers(window);
         glfwPollEvents();
 
